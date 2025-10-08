2660adf78f98aa8dd42c606858343338191a1d72
Corentin Busi <corentinbusi@gmail.com>
2025-10-08 10:48:22 +0200

    Ajout des bullet points + finalisation animations au clic

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,33 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dots = document.querySelectorAll(".dot")
+const bannerImg = document.querySelector(".banner-img")
+const bannerText = document.querySelector("#banner p")
+
+let i = 0;
+
+function updateSlide() {
+	bannerImg.src = `./assets/images/slideshow/${slides[i].image}`;
+	bannerText.innerHTML = slides[i].tagLine;
+	dots.forEach((dot, index) => {
+		dot.classList.toggle("dot_selected", index === i);
+	});
+}
+
+arrowRight.addEventListener("click", () => {
+	i++;
+	if (i >= slides.length) i = 0;
+	updateSlide();
+});
+
+arrowLeft.addEventListener("click", () => {
+	i--;
+	if (i < 0) i = slides.length - 1;
+	updateSlide();
+});
+
+updateSlide();
